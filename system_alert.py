d3de42cecf60438c6216afa3567371e49bbd0d48
mundotv789123 <48911860+mundotv789123@users.noreply.github.com>
2021-04-08 22:28:58 -0300

    Update system_alert.py

--- system_alert.py
+++ system_alert.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# sistema de alerta para gerenciamento de nodes
-# aqui você pode configurar um script que fique rodando em um crontab e alerta quando algum recursos é usado mais do que o necessário
+# Sistema de alerta para gerenciamento de nodes
+# Aqui você pode configurar um script que fique rodando em um crontab e alerta quando algum recursos é usado mais do que o necessário
 
 import requests
 import psutil
@@ -16,7 +16,7 @@ def send_notification(message):
     print('Error while send wenhook request')
 
 # Configurações
-discord_url_webhook='' # insira o webhook do discord aqui para receber as notificações
+discord_url_webhook='' # Insira o webhook do discord aqui para receber as notificações
 name='Node 01'
 
 # Limites
@@ -24,17 +24,17 @@ hdd_porcent_alert = 90
 ram_porcent_alert = 90
 load_alert = 20
 
-#alerta de memória hd
+# Alerta de hd
 hdd = psutil.disk_usage('/')
 if (hdd.percent >= hdd_porcent_alert):
   send_notification('@everyone **{0}** Ultrapassou {1}% de HD {2}%'.format(name, hdd_porcent_alert, hdd.percent))
 
-#alerta de memória ram
+# Alerta de memória ram
 ram = psutil.virtual_memory()
 if (ram.percent >= ram_porcent_alert):
   send_notification('@everyone **{0}** Ultrapassou {1}% de RAM {2}%'.format(name, ram_porcent_alert, ram.percent))
 
-#alert cpu load average
+# Alerta cpu load average
 load1, load5, load15 = os.getloadavg()
 if (load1 >= load_alert):
   send_notification('@everyone **{0}** Ultrapassou {1} de Load Average {2}'.format(name, load_alert, load1))
