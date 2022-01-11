2ad40c382dadbf5e3882ad2d782d16d2209088d9
Michael Fernando <48911860+mundotv789123@users.noreply.github.com>
2022-01-11 09:14:38 -0300

    Adding network speed check

--- system_alert.py
+++ system_alert.py
@@ -14,6 +14,11 @@ load_alert = 20
 hds = {
   'Principal': '/'
 }
+
+#lista de interfaces de rede
+interfaces = {
+  'eth0': 512 #em mbps
+}
 #=====================================================
 
 import requests
@@ -33,22 +38,41 @@ def send_notification(message):
 def check_hd(hd_name, path):
   hdd = psutil.disk_usage(path)
   if (hdd.percent >= hdd_porcent_alert):
-    send_notification('@everyone **{0}** Ultrapassou {1}% de HD ({2}) {3}%'.format(name, hdd_porcent_alert, hd_name, hdd.percent))
+    send_notification('@everyone **{0}** Ultrapassou `{1}%` de HD ({2}) `{3}%`'.format(name, hdd_porcent_alert, hd_name, hdd.percent))
 
 def check_ram():
   ram = psutil.virtual_memory()
   if (ram.percent >= ram_porcent_alert):
-    send_notification('@everyone **{0}** Ultrapassou {1}% de RAM {2}%'.format(name, ram_porcent_alert, ram.percent))
+    send_notification('@everyone **{0}** Ultrapassou `{1}%` de RAM `{2}%`'.format(name, ram_porcent_alert, ram.percent))
 
 def check_load():
   load1, load5, load15 = os.getloadavg()
   if (load1 >= load_alert):
-    send_notification('@everyone **{0}** Ultrapassou {1} de Load Average {2}'.format(name, load_alert, load1))
+    send_notification('@everyone **{0}** Ultrapassou `{1}` de Load Average `{2}`'.format(name, load_alert, load1))
 
-#executando funções
+def check_net_speed(interface, limit):
+  counters = psutil.net_io_counters(pernic=True)[interface]
+  time.sleep(1)
+  counters2 = psutil.net_io_counters(pernic=True)[interface]
+  
+  download=int(((counters2.bytes_recv - counters.bytes_recv)/1024/1024)*8)
+  upload=int(((counters2.bytes_sent - counters.bytes_sent)/1024/1024)*8)
+  
+  if (download > limit):
+    send_notification('@everyone **{0}** Ultrapassou `{1} mbps` de Download `{2} mbps`'.format(name, limit, download))
+  if (upload > limit):
+    send_notification('@everyone **{0}** Ultrapassou `{1} mbps` de Upload `{2} mbps`'.format(name, limit, upload))
+ 
+#checking hds
 for hd in hds:
   check_hd(hd, hds[hd])
 
+#checking ram
 check_ram()
 
+#checking cpu load
 check_load()
+
+#checking network speed
+for interface in interfaces:
+  check_net_speed(interface, interfaces[interface])
