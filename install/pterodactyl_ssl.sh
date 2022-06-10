4db31dc9b31028a98d81343ce4200ee0f89248af
Michael Fernando <48911860+mundotv789123@users.noreply.github.com>
2022-06-10 04:30:24 -0300

    Update

--- install/pterodactyl_ssl.sh
+++ install/pterodactyl_ssl.sh
@@ -212,7 +212,7 @@ curl -sSL https://get.docker.com/ | CHANNEL=stable bash
 systemctl enable --now docker
 
 mkdir -p /etc/pterodactyl
-curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 chmod u+x /usr/local/bin/wings
 
 echo "
