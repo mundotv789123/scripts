ca8b900cb05501986d275afe1b4e8e8b1800482e
Michael Fernandes <789michaelfernandes789@gmail.com>
2023-02-10 10:02:54 -0300

    Corrigindo a endentação

--- automação/update_wings_all_nodes.sh
+++ automação/update_wings_all_nodes.sh
@@ -8,28 +8,28 @@
 
 ssh_key_file=".ssh/id_rsa"
 nodes=(
-	'node.exemplo.com.br'
-	'node2.exemplo.com.br'
+  'node.exemplo.com.br'
+  'node2.exemplo.com.br'
 )
 
 for node in "${nodes[@]}"; do
-	echo "Instalando $ip ..."
+  echo "Instalando $ip ..."
   while true; do
     # Conectando no ssh e executando comandos para atualizacao.
-		ssh -i $ssh_key_file \
-		-o PreferredAuthentications=publickey \
-		-o StrictHostKeyChecking=no \
-		-o ConnectTimeout=10 \
-		root@$node <<EOF
-	    systemctl stop wings;
-	    curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_\$([[ "\$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")";
-	    chmod u+x /usr/local/bin/wings;
-	    systemctl start wings;
+    ssh -i $ssh_key_file \
+    -o PreferredAuthentications=publickey \
+    -o StrictHostKeyChecking=no \
+    -o ConnectTimeout=10 \
+    root@$node <<EOF
+      systemctl stop wings;
+      curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_\$([[ "\$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")";
+      chmod u+x /usr/local/bin/wings;
+      systemctl start wings;
 EOF
 
     # Verificando se o comando retornou algum erro
-		if [ $? -eq 0 ]; then break; fi
-		read -p "Ocorreu um erro ao instalar o node $node! Deseja tentar novamente? [y/N]" answer
+    if [ $? -eq 0 ]; then break; fi
+    read -p "Ocorreu um erro ao instalar o node $node! Deseja tentar novamente? [y/N]" answer
     if [ "$answer" != "y" ]; then break; fi
   done
 done
