c0f595d9f708fa97945003378908251abdc47b45
Michael Fernandes <789michaelfernandes789@gmail.com>
2025-12-16 07:42:42 -0300

    format

--- backups/mariadb_incremental_backup.py
+++ backups/mariadb_incremental_backup.py
@@ -8,11 +8,11 @@ from datetime import datetime
 import os
 import shutil
 
-input('Esse script não foi testado, realmente deseja continuar?')
+input("Esse script não foi testado, realmente deseja continuar?")
 
-backup_dir='./mysql_backups'
-db_user='root'
-days_rotate=1
+backup_dir = "./mysql_backups"
+db_user = "root"
+days_rotate = 1
 
 if not os.path.exists(backup_dir):
     os.mkdir(backup_dir)
@@ -22,59 +22,70 @@ con.row_factory = sqlite3.Row
 cur = con.cursor()
 
 # iniciando banco
-cur.execute("""CREATE TABLE IF NOT EXISTS backups(
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS backups(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     parent_id INTEGER,
     path VARCHAR(512) NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(parent_id) REFERENCES backups(id) ON DELETE CASCADE
-)""")
+)"""
+)
 
 
 # manutenção
-deleteds=[]
-res = cur.execute('SELECT id, parent_id, path FROM backups ORDER BY id ASC')
-all_backups=res.fetchall()
+deleteds = []
+res = cur.execute("SELECT id, parent_id, path FROM backups ORDER BY id ASC")
+all_backups = res.fetchall()
 for backup in all_backups:
-    if not os.path.exists(backup['path']) or backup['parent_id'] in deleteds:
-        deleteds.append(backup['id'])
-        cur.execute('DELETE FROM backups WHERE id = ?', [backup['id']])
+    if not os.path.exists(backup["path"]) or backup["parent_id"] in deleteds:
+        deleteds.append(backup["id"])
+        cur.execute("DELETE FROM backups WHERE id = ?", [backup["id"]])
         con.commit()
         print(f"not found: {backup['path']}")
-        if os.path.exists(backup['path']):
-            shutil.rmtree(backup['path'])
+        if os.path.exists(backup["path"]):
+            shutil.rmtree(backup["path"])
 # backup
-res = cur.execute("""SELECT * 
+res = cur.execute(
+    """SELECT * 
     FROM backups 
     WHERE parent_id IS NULL 
     AND DATE(created_at) = CURRENT_DATE
     ORDER BY created_at 
     DESC LIMIT 1
-""")
+"""
+)
 last_full_backup = res.fetchone()
 
 current_date = datetime.now()
 
 if not last_full_backup:
-    path_to_backup = os.path.join(backup_dir, current_date.strftime('%d-%m-%Y_full'))
-    res = cur.execute('INSERT INTO backups (path) VALUES (?)', [path_to_backup])
+    path_to_backup = os.path.join(backup_dir, current_date.strftime("%d-%m-%Y_full"))
+    res = cur.execute("INSERT INTO backups (path) VALUES (?)", [path_to_backup])
     con.commit()
 
-    command = f'mariadb-backup --backup --target-dir={path_to_backup} --user={db_user}'
+    command = f"mariadb-backup --backup --target-dir={path_to_backup} --user={db_user}"
 else:
-    res = cur.execute("""SELECT * 
+    res = cur.execute(
+        """SELECT * 
         FROM backups 
         WHERE DATE(created_at) = CURRENT_DATE 
         ORDER BY created_at DESC 
         LIMIT 1
-    """)
+    """
+    )
     last_backup = res.fetchone()
-    path_last_backup = last_backup['path']
-    path_to_backup = os.path.join(backup_dir, current_date.strftime('%d-%m-%Y_%H-%M-%S'))
-    res = cur.execute('INSERT INTO backups (parent_id, path) VALUES (?, ?)', [last_backup['id'], path_to_backup])
+    path_last_backup = last_backup["path"]
+    path_to_backup = os.path.join(
+        backup_dir, current_date.strftime("%d-%m-%Y_%H-%M-%S")
+    )
+    res = cur.execute(
+        "INSERT INTO backups (parent_id, path) VALUES (?, ?)",
+        [last_backup["id"], path_to_backup],
+    )
     con.commit()
 
-    command = f'mariadb-backup --backup --target-dir={path_to_backup} --incremental-basedir={path_last_backup} --user={db_user}'
+    command = f"mariadb-backup --backup --target-dir={path_to_backup} --incremental-basedir={path_last_backup} --user={db_user}"
 
 if os.path.exists(path_to_backup):
     shutil.rmtree(path_to_backup)
@@ -82,11 +93,14 @@ if os.path.exists(path_to_backup):
 os.system(command)
 
 # expurgo
-res = cur.execute(f"SELECT * FROM backups WHERE parent_id is null AND DATE(created_at, '+{days_rotate} day') <= CURRENT_DATE")
+res = cur.execute(
+    f"SELECT * FROM backups WHERE parent_id is null AND DATE(created_at, '+{days_rotate} day') <= CURRENT_DATE"
+)
 backups_to_delete = res.fetchall()
 
 for backup in backups_to_delete:
-    res = cur.execute("""
+    res = cur.execute(
+        """
         WITH RECURSIVE tree AS (
             SELECT id, parent_id, path
             FROM backups
@@ -99,12 +113,14 @@ for backup in backups_to_delete:
             JOIN tree t ON b.parent_id = t.id
         )
         SELECT * FROM tree
-    """, [backup['id']])
+    """,
+        [backup["id"]],
+    )
     backups_childrens = res.fetchall()
     for children in backups_childrens:
-        if os.path.exists(children['path']):
-            shutil.rmtree(children['path'])
-        cur.execute('DELETE FROM backups WHERE id = ?', [children['id']])
+        if os.path.exists(children["path"]):
+            shutil.rmtree(children["path"])
+        cur.execute("DELETE FROM backups WHERE id = ?", [children["id"]])
         con.commit()
         print(f"{children['path']} {children['id']} deleted")
 
